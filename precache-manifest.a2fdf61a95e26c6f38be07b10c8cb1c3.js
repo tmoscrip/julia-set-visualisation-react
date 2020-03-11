16c1a91671e247be6c7ba3e4d21c78d12be97911
Tom Moscrip <sgtmoscr@liv.ac.uk>
2020-03-11 14:24:27 +0000

    Updates

---
+++ precache-manifest.a2fdf61a95e26c6f38be07b10c8cb1c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db1711218020af95c480861189d73aeb",
+    "url": "/julia-frontend/index.html"
+  },
+  {
+    "revision": "1a28f76ff72e32d89ca9",
+    "url": "/julia-frontend/static/css/main.dbc598f7.chunk.css"
+  },
+  {
+    "revision": "a3d9c56a6b3ef08ed41b",
+    "url": "/julia-frontend/static/js/2.14bc1028.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/julia-frontend/static/js/2.14bc1028.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1a28f76ff72e32d89ca9",
+    "url": "/julia-frontend/static/js/main.08133e53.chunk.js"
+  },
+  {
+    "revision": "a7e4c2518e8e3d0d6ed7",
+    "url": "/julia-frontend/static/js/runtime-main.a642d25b.js"
+  }
+]);
