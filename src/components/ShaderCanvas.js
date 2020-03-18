93dbc1e7099ac92d8ef936af790f97d76adfc5c9
Thomas Moscrip <tmoscrip1@gmail.com>
2020-03-18 13:53:39 +0000

    sync

--- src/components/ShaderCanvas.js
+++ src/components/ShaderCanvas.js
@@ -7,19 +7,21 @@ import ControlPanel from './ControlPanel/ControlPanel'
 export default function ShaderCanvas() {
   const ctx = useContext(ShaderContext)
 
-  const [, setCanvasRef] = ctx.canvasRef
+  const [canvasRef, setCanvasRef] = ctx.canvasRef
   const [gl, setGl] = ctx.gl
   const [paused] = ctx.time.paused
 
-  const canvasRef = useRef()
+  const canvasRefTemp = useRef()
   const animateRef = useRef()
   const [frameCount, setFrameCount] = useState(0)
   const [lastFrameTime, setLastFrameTime] = useState(0)
 
+  const [dragStart, setDragStart] = useState()
+
   // Initialise canvas and webgl
   useEffect(() => {
-    setCanvasRef(canvasRef.current)
-    setGl(canvasRef.current.getContext('webgl'))
+    setCanvasRef(canvasRefTemp.current)
+    setGl(canvasRefTemp.current.getContext('webgl'))
   }, [setCanvasRef, setGl])
 
   // Start rendering
@@ -43,9 +45,37 @@ export default function ShaderCanvas() {
     return () => cancelAnimationFrame(animateRef.current)
   }, [ctx, paused, gl])
 
+  function startDrag(e) {
+    console.log('start drag')
+    const dragStart = [e.clientX, e.clientY]
+    console.log(dragStart)
+    setDragStart(dragStart)
+  }
+
+  function endDrag(e) {
+    console.log('end drag')
+    const canvas = document.getElementsByClassName('glcanvas')[0]
+    console.log(canvas)
+    const [canvasWidth, canvasHeight] = [
+      canvas.offsetWidth, canvas.offsetHeight
+    ]
+    const [startX, startY] = dragStart
+    const [width, height] = [e.clientX - startX, e.clientY - startY]
+    console.log(width, height)
+    console.log(width/canvasWidth, height/canvasHeight)
+
+  }
+
   return (
     <>
-      <canvas className='glcanvas' width='1000' height='1000' ref={canvasRef} />
+      <canvas
+        className='glcanvas'
+        width='1000'
+        height='1000'
+        onMouseDown={startDrag}
+        onMouseUp={endDrag}
+        ref={canvasRefTemp}
+      />
       <ControlPanel>
         <DebugFrame frameCount={frameCount} frameTime={Date.now() - lastFrameTime} />
       </ControlPanel>
